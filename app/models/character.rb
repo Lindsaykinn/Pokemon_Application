438800a1b83ff9c934aa66b49484d9113aee0d66
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-03 15:05:37 -0500

    added character validation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :name, :weight, :height, :energy_type, :vip_status, :hit_points
 end
+
+
+t.string :name
+      t.integer :weight
+      t.string :height
+      t.string :energy_type
+      t.string :vip_status
+      t.integer :hit_points
+      t.integer :user_id
