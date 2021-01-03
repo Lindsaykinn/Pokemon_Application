3acde2319eb3eaa0c8573c3c1e59b2e5a83c5fde
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-03 15:03:08 -0500

    added post character controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,4 +3,8 @@ class CharactersController < ApplicationController
   get '/characters/new' do
     erb :'characters/new'
   end
+
+  post '/characters' do
+   character = Character.new(params[:character])
+  end
 end
