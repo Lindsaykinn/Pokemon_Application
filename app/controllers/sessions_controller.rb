df0cebe94542d4a2d3a1b38897fb6b1f1127a4f1
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-05 13:27:22 -0500

    added post /signup to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
+# where login/logout/signup is housed
+
 class SessionsController < ApplicationController
 
-  get '/sessions/signup' do
+  get '/signup' do
     erb :'sessions/signup'
   end
   
   # allows the sign up button to work from welcome page
   post '/signup' do
-    redirect "/sessions/signup"
+    user = User.new(params[:user])
+   
+    if user.save
+      #this line logs in the user:
+      session[:user_id] = user.id
+      redirect "/characters"
+    else
+      redirect "/signup"
+    end
   end
-
 
 end
