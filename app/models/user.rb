6ed4e07ba6ed58e237692c9c2a6c745c9ce806b7
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-05 13:27:53 -0500

    added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :characters
+  #macro provided by bcrypt gem. encrypts the password and stores the password as password_digest:
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
 end
